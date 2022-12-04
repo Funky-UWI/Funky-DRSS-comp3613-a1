92c26b7a64626b99e04a0bfcfb0fc9a44da41ad9
Ved Mahadeo <vedkailash1@gmail.com>
2022-12-04 13:58:28 -0400

    Revert "added cascade delete to votecommand"
    This reverts commit be0776d425d960291b35d937be33c4eccccdbc58.

--- App/models/review.py
+++ App/models/review.py
@@ -11,14 +11,26 @@ class Review(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     student_id = db.Column(db.Integer, db.ForeignKey("student.id"), nullable=False)
     text = db.Column(db.String(1000), nullable=False)
-    votes = db.relationship('VoteCommand', back_populates="review", cascade="all, delete")
-
+    votes = db.Column(MutableDict.as_mutable(JSON), nullable=False)
 
     def __init__(self, user_id, student_id, text):
         self.user_id = user_id
         self.student_id = student_id
         self.text = text
+        self.votes = {"num_upvotes": 0, "num_downvotes": 0}
 
+    def vote(self, user_id, vote):
+        self.votes.update({user_id: vote})
+        self.votes.update(
+            {"num_upvotes": len([vote for vote in self.votes.values() if vote == "up"])}
+        )
+        self.votes.update(
+            {
+                "num_downvotes": len(
+                    [vote for vote in self.votes.values() if vote == "down"]
+                )
+            }
+        )
 
     def get_num_upvotes(self):
         num_upvotes=0
